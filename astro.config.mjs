aabfdd7b88a34b3874bdba4020118389a38b892b
Suhas Dissanayake <suhasdissa@gmail.com>
2025-01-02 00:46:03 +0530

    Fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://suhasdissa.top",
-  base: "/srilanka-history-timeline",
   integrations: [mdx(), sitemap(), tailwind()],
 });
