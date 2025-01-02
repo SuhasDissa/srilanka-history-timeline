1286310d1bc681a73cc13e51ec3338339e6b45e1
Suhas Dissanayake <suhasdissa@gmail.com>
2025-01-02 13:13:48 +0530

    Fix site metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://suhasdissa.top",
+  site: "https://history.suhasdissa.top",
   integrations: [mdx(), sitemap(), tailwind()],
 });
